7d2c23754a604a6ad27cf72c24c186c218af9f28
williiamdr <williiamvlog@gmail.com>
2024-02-06 20:35:13 -0800

    Update1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Flash-Game
+
+# Requirements
+
+- Object/img
+- Score
+- Main.js
+- Index.html
+- Index.css
+
+# Game Goal 
+
+- Avoid object/image get hit by a moving object
+- Run as much as you can 
---
+++ main.js
@@ -0,0 +1,6 @@
+// Create a funnction where luigi.png is showing
+// Create a function where enemies/objects move from one direction to another
+// Create a function for scoring 
+// Create a function for when luigi.png collides with enemies/objects 
+// Create a function to make luigi.png jump 
+// Create a function when game is over 
