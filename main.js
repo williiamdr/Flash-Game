aae782e0d6ca0584af931ef97fedcd2987a01daf
williiamdr <williiamvlog@gmail.com>
2024-02-09 20:32:56 -0800

    decided to start over :C

--- main.js
+++ main.js
@@ -4,3 +4,20 @@
 // Create a function for when luigi.png collides with enemies/objects 
 // Create a function to make luigi.png jump 
 // Create a function when game is over 
+
+const luigi = document.getElementById("luigi");
+const bullet = document.getElementById("bullet");
+
+function jump (){
+    luigi.classList.add("jump");
+
+    setTimeout(function (){
+        luigi.classList.remove("jump");
+    }, 300);
+}
+
+
+document.addEventListener("keydown", function(event) {
+jump();
+})
+
